7316af754da93dc85f85e213e2065651dd64d82d
Challenger2 <challenger2@users.noreply.github.com>
2015-12-13 19:02:06 -0600

    Removed all NMS dependences

--- src/org/challenger2/BiomeReplacer/BiomeReplacer.java
+++ src/org/challenger2/BiomeReplacer/BiomeReplacer.java
@@ -1,10 +1,11 @@
 package org.challenger2.BiomeReplacer;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
-import net.minecraft.server.v1_8_R3.PacketPlayOutMapChunk.ChunkMap;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -16,7 +17,7 @@ import com.comphenix.protocol.reflect.StructureModifier;
 public class BiomeReplacer extends JavaPlugin {
 	
 	private final int BIOME_ARRAY_SIZE = 256;
-	
+	private static final Logger log = Logger.getLogger("Minecraft");
 	
 	private class MyPacketAdapter extends PacketAdapter {
 			public MyPacketAdapter(BiomeReplacer p) {
@@ -56,33 +57,98 @@ public class BiomeReplacer extends JavaPlugin {
 		adapter = null;
 	}
 
+	/**
+	 * Translate a Chunk object to a new biome
+	 * 
+	 * event.getPacket gets an NMS object with the following structure
+	 * {
+     *   private int a;
+     *   private int b;
+     *   private PacketPlayOutMapChunk.ChunkMap c;
+     *   private boolean d;
+	 * }
+	 * Here, field 3 has the data we want.
+	 * 
+	 * After we get a ChunkMap object, we use reflection to get the data out of it
+	 * A ChunkMap looks like this:
+	 * 
+	 * public static class ChunkMap {
+     *   public byte[] a;
+     *   public int b;
+     *   public ChunkMap() {}
+     * }
+     * 
+     * Here, a is the raw chunk data and b is the primaryMask
+     * 
+     * The biome data is always the last 256 bytes of the chunk data, if
+     * the hasContinous flag is set (field d)
+	 * 
+	 * @param event
+	 */
 	private void TranslateChunk(PacketEvent event)
 	{
-		PacketContainer packet = event.getPacket();
+		try{
+			PacketContainer packet = event.getPacket();
+			Object chunk = packet.getModifier().read(2); // grab field 'c'
+			Field[] fields = chunk.getClass().getDeclaredFields(); // get all the fields in the chunk
+			byte[] data = byte[].class.cast(fields[0].get(chunk)); // read the first field from the chunk
+	
+			StructureModifier<Boolean> bools = packet.getBooleans(); // Read field 'd', hasContinous
+			boolean hasContinous = bools.read(0);
 
-		ChunkMap chunk = (ChunkMap)packet.getModifier().read(2);
-		byte[] data = chunk.a;
-		//int primaryBitMask = chunk.b;
-
-		StructureModifier<Boolean> bools = packet.getBooleans();
-		boolean hasContinous = bools.read(0);
-
-		if (hasContinous) {
-			ReplaceBiome(data);
+			// Biome data is only present if this flag is set.
+			if (hasContinous) {
+				ReplaceBiome(data);
+			}
+		} catch (IllegalAccessException e) {
+			log.severe("Failed to get single chunk data:\n" + e.toString());
 		}
 	}
 
+	/**
+	 * translate a Chunk Builk object to a new biome
+	 * 
+	 * We use event.getPacket to get a structure like this one:
+	 * {
+	 *  private int[] a;
+     *	private int[] b;
+     *	private PacketPlayOutMapChunk.ChunkMap[] c;
+     *	private boolean d;
+     *	private World world; // Spigot
+	 * }
+	 * 
+	 * What we want is field c, a ChunkMap array
+	 * 
+	 * After we get a ChunkMap object, we use reflection to get the data out of it
+	 * A ChunkMap looks like this:
+	 * 
+	 * public static class ChunkMap {
+     *   public byte[] a;
+     *   public int b;
+     *   public ChunkMap() {}
+     * }
+     * 
+     * Here, a is the raw chunk data and b is the primaryMask
+     * 
+     * The biome data is always the last 256 bytes of the chunk data
+	 * 
+	 * @param event
+	 */
 	private void TranslateBulk(PacketEvent event)
 	{
-		PacketContainer packet = event.getPacket();
-
-		ChunkMap[] chunks = (ChunkMap[])packet.getModifier().read(2);
-
-		for (ChunkMap chunk : chunks) {
-			byte[] data = chunk.a;
-			// int primaryBitMask = chunk.b;
-
-			ReplaceBiome(data);
+		try {
+			PacketContainer packet = event.getPacket();
+			Object chunkArray = packet.getModifier().read(2);
+			int numChunks = Array.getLength(chunkArray);
+			
+			for (int i = 0; i < numChunks; i++) {
+				Object chunk = Array.get(chunkArray, i);
+				Field[] fields = chunk.getClass().getDeclaredFields();
+				byte[] data = byte[].class.cast(fields[0].get(chunk));
+				ReplaceBiome(data);
+			}
+		} catch (IllegalAccessException e) {
+			log.severe("Failed to get bunk chunk data:\n" + e.toString());
 		}
 	}
 
@@ -93,42 +159,4 @@ public class BiomeReplacer extends JavaPlugin {
 		}
 	}
 
-
-	/*
-	 *  This one works!!!
-	 *
-	private void TranslateBulk(PacketEvent event)
-	{
-
-		PacketContainer packet = event.getPacket();
-
-		ChunkMap[] chunks = (ChunkMap[])packet.getModifier().read(2);
-
-		for (ChunkMap chunk : chunks) {
-			byte[] data = chunk.a;
-			int length = chunk.b;
-			
-			if (length > data.length) {
-				length = data.length;
-			}
-	
-			StructureModifier<Boolean> bools = packet.getBooleans();
-			Boolean hasContinousObj = bools.readSafely(0);
-			boolean hasContinous;
-			if (hasContinousObj  == null) {
-				hasContinous = true;
-			} else {
-				hasContinous = hasContinousObj;
-			}
-	
-			if (hasContinous && data.length > BIOME_ARRAY_SIZE) {
-				// There should be biome data if chunk mask is nonzero
-				for (int i = data.length - BIOME_ARRAY_SIZE; i < data.length; i++) {
-					data[i] = (byte)140; // Ice Spikes
-				}
-			}
-		}
-	}
-	*/
-	
 }
